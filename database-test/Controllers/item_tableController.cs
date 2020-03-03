06b3f36b454e6a89fc5bff5ca7f56dee6ce5c26d
Ben Lokos <Ben_lokos@hotmail.com>
2020-03-02 20:46:30 -0600

    Add project files.

---
+++ database-test/Controllers/item_tableController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using database_test.Models;
+
+namespace database_test.Controllers
+{
+    public class item_tableController : Controller
+    {
+        private MYFIRSTEntities db = new MYFIRSTEntities();
+
+        // GET: item_table
+        public ActionResult Index()
+        {
+            var item_tables = db.item_tables.Include(i => i.trader_index);
+            return View(item_tables.ToList());
+        }
+
+        // GET: item_table/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item_table item_table = db.item_tables.Find(id);
+            if (item_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item_table);
+        }
+
+        // GET: item_table/Create
+        public ActionResult Create()
+        {
+            ViewBag.itemownerID = new SelectList(db.trader_indices, "traderID", "firstname");
+            return View();
+        }
+
+        // POST: item_table/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "itemname,itemownerID")] item_table item_table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.item_tables.Add(item_table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.itemownerID = new SelectList(db.trader_indices, "traderID", "firstname", item_table.itemownerID);
+            return View(item_table);
+        }
+
+        // GET: item_table/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item_table item_table = db.item_tables.Find(id);
+            if (item_table == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.itemownerID = new SelectList(db.trader_indices, "traderID", "firstname", item_table.itemownerID);
+            return View(item_table);
+        }
+
+        // POST: item_table/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "itemname,itemownerID")] item_table item_table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item_table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.itemownerID = new SelectList(db.trader_indices, "traderID", "firstname", item_table.itemownerID);
+            return View(item_table);
+        }
+
+        // GET: item_table/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item_table item_table = db.item_tables.Find(id);
+            if (item_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item_table);
+        }
+
+        // POST: item_table/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            item_table item_table = db.item_tables.Find(id);
+            db.item_tables.Remove(item_table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ database-test/Controllers/trader_indexController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using database_test.Models;
+
+namespace database_test.Controllers
+{
+    public class trader_indexController : Controller
+    {
+        private MYFIRSTEntities db = new MYFIRSTEntities();
+
+        // GET: trader_index
+        public ActionResult Index()
+        {
+            return View(db.trader_indices.ToList());
+        }
+
+        // GET: trader_index/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            trader_index trader_index = db.trader_indices.Find(id);
+            if (trader_index == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trader_index);
+        }
+
+        // GET: trader_index/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: trader_index/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "traderID,firstname,lastname,email")] trader_index trader_index)
+        {
+            if (ModelState.IsValid)
+            {
+                db.trader_indices.Add(trader_index);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trader_index);
+        }
+
+        // GET: trader_index/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            trader_index trader_index = db.trader_indices.Find(id);
+            if (trader_index == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trader_index);
+        }
+
+        // POST: trader_index/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "traderID,firstname,lastname,email")] trader_index trader_index)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trader_index).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trader_index);
+        }
+
+        // GET: trader_index/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            trader_index trader_index = db.trader_indices.Find(id);
+            if (trader_index == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trader_index);
+        }
+
+        // POST: trader_index/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            trader_index trader_index = db.trader_indices.Find(id);
+            db.trader_indices.Remove(trader_index);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
